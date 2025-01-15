2020f6e8d92f382ab3000cb2ecb5f8fbfb0fd826
Aaron <huichiao.tsou@gmail.com>
2025-01-15 15:21:05 +0800

    新增刪除帳號

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // 修改路由
-app.use('/privacyterms', express.static('public'));
+app.use("/privacyterms", express.static("public"));
 
-app.get('/', (req, res) => {
-    res.redirect("https://thehope.co");
+app.get("/", (req, res) => {
+  res.redirect("https://thehope.co");
 });
 
-// 其他 API 或路由（可選）
-app.get('/zh/privacyterms', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'privacy.html'));
+app.get("/zh/privacyterms", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "privacy.html"));
 });
 
-// 其他 API 或路由（可選）
-app.get('/en/privacyterms', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'privacy_en.html'));
+app.get("/en/privacyterms", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "privacy_en.html"));
+});
+
+app.get("/zh/deleteaccount", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "deleteaccount.html"));
+});
+
+app.get("/en/deleteaccount", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "deleteaccount_en.html"));
 });
 
 // 啟動伺服器
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
